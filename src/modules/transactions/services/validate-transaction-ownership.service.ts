18d65dd1074a551776b3497399397c344db13ef0
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-08-27 20:50:20 -0300

    feat: change outDir to public

--- src/modules/transactions/services/validate-transaction-ownership.service.ts
+++ src/modules/transactions/services/validate-transaction-ownership.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repositories';
-
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { TransactionsRepository } from "src/shared/database/repositories/transactions.repositories";
 
 @Injectable()
 export class validateTransactionOwnershipService {
-  constructor(private readonly transacitonRepo: TransactionsRepository){}
+  constructor(private readonly transacitonRepo: TransactionsRepository) {}
 
-  async validate(userId: string, transactionId: string){
-    const isOwner = await this.transacitonRepo.findFirst({where: {userId, id: transactionId}})
+  async validate(userId: string, transactionId: string) {
+    const isOwner = await this.transacitonRepo.findFirst({
+      where: { userId, id: transactionId },
+    });
 
-    if(!isOwner){
-      throw new NotFoundException('Transação não encontrada')
+    if (!isOwner) {
+      throw new NotFoundException("Transação não encontrada");
     }
   }
 }
