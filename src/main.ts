24f8a87feb029962ed55d78b1777f693ae6672dc
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-11-14 18:23:31 -0300

    feat: changes on vercel json

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import "dotenv/config";
 import { AppModule } from "./app.module";
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from "@nestjs/platform-express";
+import * as express from "express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(express())
+  );
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: "*",
   });
-  await app.listen(3000);
+
+  await app.init();
 }
+
 bootstrap();
+
+// Exportando a função que será utilizada pelo Vercel
+export default async (req, res) => {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  await app.init();
+  return app.getHttpAdapter().getInstance()(req, res); // Aqui exportamos o handler correto para o Vercel
+};
